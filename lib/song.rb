d45bd1d27b09c7d5dc804954b0ed669bae949f69
ionwolf13 <voli.scorp13@gmail.com>
2020-11-22 07:40:54 -0600

    commit tree c7fb028f5715823cf5a954ee97b86028a6955a66

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,58 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+    def self.create
+      song1 = self.new
+      song1.save
+      if self.all.include?(song1)
+              return song1
+      end
+    end
+
+    def self.new_by_name(name)
+      song = Song.new(name)
+      song.name = name
+      if self.all.include?(song)
+        return song
+      end
+    end
+
+    def self.create_by_name
+      @name = name
+      song = Song.new(name)
+      song.save
+      if self.all.include?(song)
+        return song
+      end
+    end
+
+    def self.find_by_name
+
+    end
+
+    def self.find_or_create_by_name
+
+    end
+
+    def self.alphabetical
+        array = self.all.map do |song| 
+          song.name
+        end
+        array.sort
+    end
+
+    def self.new_from_filename
+
+    end
+
+    def self.create_from_filename
+
+    end
+
+    def self.destroy_all
+      self.all.clear()
+    end
+
   def self.all
     @@all
   end
@@ -9,5 +61,7 @@ class Song
   def save
     self.class.all << self
   end
+
+
 
 end
